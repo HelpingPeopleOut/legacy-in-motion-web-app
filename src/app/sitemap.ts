c7a422a6425d28725bbb47de310b7e16a8283b69
Jose Lara <hpo@hpo.center>
2026-05-12 03:43:08 -0700

    Fix: Forced robots and sitemap to build statically for Cloudflare Pages export

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,5 +1,7 @@
 import { MetadataRoute } from 'next';
 
+export const dynamic = "force-static"; // <-- ADDED THIS LINE
+
 export default function sitemap(): MetadataRoute.Sitemap {
   const baseUrl = 'https://test-legacy-in-motion-web-app.pages.dev'; // Change to https://www.legacyinmotion.org when you go live!
 
